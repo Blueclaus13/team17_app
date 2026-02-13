e2d4226a4b348065b7053b850871fe3f5994890d
Claudia Madrid <claudia.madrid@choicehotels.com>
2026-02-12 22:29:41 -0600

    ChangedEmunDisplayedDB

--- MindfulMomentsApp/Data/AppDbContext.cs
+++ MindfulMomentsApp/Data/AppDbContext.cs
@@ -38,6 +38,10 @@ namespace MindfulMomentsApp.Data
       {
         entity.HasKey(e => e.EntryId);
         entity.Property(e => e.Description).HasMaxLength(1000);
+        entity.Property(e => e.Mood)
+          .HasConversion<string>();
+        entity.Property(e => e.Activity)
+          .HasConversion<string>();
         entity.HasOne(e => e.Journal)
                   .WithMany(j => j.Entries)
                   .HasForeignKey(e => e.JournalId)

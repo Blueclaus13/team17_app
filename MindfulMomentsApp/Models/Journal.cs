1ccb9e64d1c7e793bbc5884d5fbaadc9b83324cd
jicmette <jicmettesc@gmail.com>
2026-02-14 14:01:23 -0600

    fix: resolve journal relationship conflicts and sync account stats

--- MindfulMomentsApp/Models/Journal.cs
+++ MindfulMomentsApp/Models/Journal.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 namespace MindfulMomentsApp.Models
 {
   public class Journal
@@ -5,6 +6,9 @@ namespace MindfulMomentsApp.Models
     public int JournalId { get; set; }
     public string JournalName { get; set; } = string.Empty;
     public Guid UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual User? User { get; set; }
 
     public virtual ICollection<Entry> Entries { get; set; } = new List<Entry>();
   }
